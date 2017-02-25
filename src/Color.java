c91fa8a014d5c1664b6afa7081e0b60ea874d1d1
Mark Schlax <mcschlax@mtu.edu>
2017-02-25 00:16:31 -0500

    Initial

---
+++ src/Color.java
@@ -0,0 +1,48 @@
+public class Color {
+    private final double r;
+    private final double g;
+    private final double b;
+
+    public Color(double r, double g, double b) {
+        assert (r < 0 || r > 1);
+        assert (g < 0 || g > 1);
+        assert (b < 0 || b > 1);
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public Color(Color c) {
+        this.r = c.r;
+        this.g = c.g;
+        this.b = c.b;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getG() {
+        return g;
+    }
+
+    public int encodeRGBGamma() {
+        int encoded;
+
+        /*
+        if rgb <= .00304
+            encode << 0 12.92 x r
+            encode << 8 12.92 x g
+            encode << 16 12.92 x b
+         else
+            encode << 0
+
+         */
+
+        return encoded;
+    }
+}
