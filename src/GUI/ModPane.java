6e6bed8dfa50004afd385d2961a591583c62c743
Domenic Portuesi <dportuesi@gmail.com>
2017-02-26 01:05:21 -0500

    Pane stuff

--- src/GUI/ModPane.java
+++ src/GUI/ModPane.java
@@ -1,8 +1,10 @@
 package GUI;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SplitPane;
@@ -42,34 +44,57 @@ public class ModPane
 		pane.setMinSize(100, root.getHeight());
 		
 		VBox left = new VBox(20);
-		left.setMinWidth(100);
+		left.setMinWidth(200);
 		left.setPrefWidth(300);
 		left.setMaxWidth(800);
 		
 		Label title = new Label("Modification Pane");
-		title.setPadding(new Insets(20));
+		title.setPadding(new Insets(20,20,5,20));
 		title.setFont(new Font("Arial", 20));
 
 		Label spacer = new Label("");
-		spacer.setPadding(new Insets(25));
-		spacer.setFont(new Font("Arial", 20));
+		//spacer.setPadding(new Insets(10));
+		//spacer.setFont(new Font("Arial", 20));
 		
 		left.getChildren().addAll(title,spacer);
 
+		HBox buttons = new HBox();
+		buttons.setPadding(new Insets(20,20,20,20));
+		
+		Button random = new Button("Randomize");
+		random.setPadding(new Insets(10,12,10,12));
+		random.setOnAction(e -> {
+			for(TextField f : textFields)
+			{
+				Random rand = new Random();
+				f.setText(Double.toString(rand.nextDouble() * 100));
+			}
+		});
+		
+		Button regen = new Button("Regenerate Image");
+		regen.setPadding(new Insets(10,12,10,12));
+		regen.setOnAction(e -> {
+			//TODO Make a regen function in Viewer
+		});
+		
+		buttons.getChildren().addAll(random,regen);
+		left.getChildren().add(buttons);
+		
 		for(int i = 0; i < 4; i++)
 		{
 			HBox fields = new HBox();
 			
 			TextField t = new TextField();
+			t.setPrefWidth(45);
 			t.setPadding(new Insets(5));
 			textFields.add(t);
 			
 			Label l = new Label("V" + i);
-			l.setPadding(new Insets(0, 5, 0, 0));
+			l.setPadding(new Insets(0, 5, 0, 20));
 			
 			fields.getChildren().addAll(l,t);
 			left.getChildren().add(fields);
-		}
+		}	
 		
 		VBox right = new VBox(5);
 		right.setMinWidth(100);
